3c30d4dfcbb85b5940f5eb0398f40c63eb3ca932
Gessica <santos.gessicacarolina@gmail.com>
2023-05-31 15:54:31 -0300

    feat: create post, get timeline and users posts functionalities

---
+++ src/repositories/hashtag.repositories.js
@@ -0,0 +1,11 @@
+import { db } from "../database/database.connect.js";
+
+export async function saveHashtag(text, postId) {
+    const regex = /#(\w+)/g;
+    let match;
+  
+    while ((match = regex.exec(text)) !== null) {
+      const tags = match[1];
+      await db.query(`INSERT INTO hashtags ("postId", tags) VALUES ($1, $2)`, [postId, tags])
+    }
+}
---
+++ src/repositories/posts.repositories.js
@@ -0,0 +1,86 @@
+import { db } from "../database/database.connect.js";
+
+export async function createNewPost(body, userId) {
+    const { link, description } = body;
+    await db.query(
+        `INSERT INTO posts ("userId", link, description) VALUES ($1, $2, $3)`,
+        [userId, link, description]
+      );
+}
+
+export function findPostByDescription(description) {
+    return db.query(
+        `SELECT * FROM posts WHERE description=$1`,
+        [description]
+      );
+}
+
+export function getPosts(){
+    return db.query(`
+    SELECT
+    posts.*,
+    users.username,
+    users."userPicture",
+    COUNT(likes."postId") AS likesCount,
+    (
+        SELECT
+            JSON_AGG(
+                JSON_BUILD_OBJECT('name', users.username)
+            )
+        FROM
+            likes
+            JOIN users ON users.id = likes."userId"
+        WHERE
+            likes."postId" = posts.id
+        GROUP BY
+            posts.id
+    ) AS likedBy
+FROM
+    posts
+JOIN users ON users.id = posts."userId"
+LEFT JOIN likes ON likes."postId" = posts.id
+GROUP BY
+    posts.id,
+    users.username,
+    users."userPicture"
+ORDER BY
+    posts."createdAt" DESC
+LIMIT 20;`);
+}
+
+export function getUserPosts(userId){
+    return db.query(
+        `
+      SELECT
+          posts.*,
+          users.username,
+          users."userPicture",
+          COUNT(likes."postId") AS likesCount,
+          (
+              SELECT
+                  JSON_AGG(
+                      JSON_BUILD_OBJECT('name', users.username)
+                  )
+              FROM
+                  likes
+              JOIN users ON users.id = likes."userId"
+              WHERE
+                  likes."postId" = posts.id
+              GROUP BY
+                  posts.id
+          ) AS likedBy
+      FROM
+          posts
+      JOIN users ON users.id = posts."userId"
+      LEFT JOIN likes ON likes."postId" = posts.id
+      WHERE posts."userId" = $1
+      GROUP BY
+          posts.id,
+          users.username,
+          users."userPicture"
+      ORDER BY
+          posts."createdAt" DESC
+      LIMIT 20;`,
+        [userId]
+      );
+}
