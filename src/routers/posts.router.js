b3bab7bc76111537304414a9864f9028e036e6af
Gessica <santos.gessicacarolina@gmail.com>
2023-05-31 19:38:48 -0300

    feat: add auth validation on posts routes

--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import validateSchema from "../middlewares/validateSchema.middleware.js";
 import postsSchema from "../schemas/postsSchema.schema.js";
 import { createPost, getAllPosts, getPostsByUserId } from "../controllers/posts.controllers.js";
+import { authValidation } from "../middlewares/authValidation.middlewares.js";
 
 const postRouter = Router();
 
+postRouter.use(authValidation);
 postRouter.post('/new-post', validateSchema(postsSchema), createPost);
 postRouter.get('/timeline', getAllPosts);
 postRouter.get('/user/:id', getPostsByUserId);
